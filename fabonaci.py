54407eda30e7cf37a7f48633294704613c099888
gegner2015 <xuebin.zhu@gmx.de>
2020-08-06 06:13:00 +0200

    no.1

---
+++ fabonaci.py
@@ -0,0 +1,18 @@
+def fabonaci(n):
+    n1=1
+    n2=1
+    n3=1
+
+    if(n<1):
+        return -1
+    while(n-2)>0:
+        n3=n1+n2
+        n1=n2
+        n2=n3
+
+        n=n-1
+
+    return n3
+
+sum=fabonaci(20)
+print (sum)
